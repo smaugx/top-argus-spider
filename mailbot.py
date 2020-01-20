7fb93d0958553b097c6f285a9a3eb5586a6403f4
smaugx <linuxcode2niki@gmail.com>
2020-01-20 22:26:08 +0800

    using selenium to spider topargus; ok

---
+++ mailbot.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+'''
+yagmail to send mail
+imapclient to download mail
+'''
+
+
+import yagmail
+import imaplib
+import email
+from email import generator
+from io import StringIO
+import mailparser
+import json
+import os
+import pdb
+
+class MailBot(object):
+    def __init__(self):
+        self.sender = None
+        self.downloader = None
+        self.username = 'bot2mail@163.com'
+        self.username = '1763283228@qq.com'
+        self.password = 'bot666'
+        self.password = 'tiercxfoarxveaad'
+        self.smtp_host = 'smtp.163.com'
+        self.smtp_host = 'smtp.qq.com'
+        self.smtp_port = 465
+        self.imap_host = 'imap.163.com'
+        self.imap_host = 'imap.qq.com'
+        self.imap_port = 993
+        return
+
+    def login(self, trytimes = 4):
+        if not self.downloader_login(trytimes):
+            print("mail downloader login fialed")
+            return False
+        if not self.sender_login(trytimes):
+            print("mail sender login failed")
+            return False
+    
+        print("mail login success")
+        return True
+
+    # imap recv mail
+    def downloader_login(self, trytimes = 3):
+        for i in range(0, trytimes):
+            try:
+                downloader = imaplib.IMAP4_SSL(self.imap_host)
+                downloader.login(self.username, self.password)
+                print('downloader login {0} success'.format(self.imap_host))
+                self.downloader = downloader
+                return True
+            except Exception as e:
+                print('downloader login {0} failed'.format(self.imap_host), e)
+
+        return False
+ 
+    # smtp send mail
+    def sender_login(self, trytimes = 3):
+        for i in range(0, trytimes):
+            try:
+                yag = yagmail.SMTP(user = self.username, password = self.password, host = self.smtp_host, port = self.smtp_port)
+                self.sender = yag
+                print("sender login success to {0}".format(self.smtp_host))
+                return True
+            except Exception as e:
+                print("sender login failed", e)
+
+        return False
+    
+    def make_pic_inline(self, pic_path):
+        return yagmail.inline(pic_path)
+    
+    def send_mail(self, email_adr, subject, contents):
+        '''
+        contents = [
+                'This is the body, and here is just text http://somedomain/image.png',
+                'You can find an audio file attached.',
+                './chuxianyoulikai_yuxi.mp3',
+                yagmail.inline('./cat1.jpg')]
+        '''
+        if not self.sender:
+            print("mail_sender not logining")
+            return False
+        try:
+            self.sender.send(email_adr, subject, contents)
+            print("send email to {0} success".format(email_adr))
+            return True
+        except Exception as e:
+            print("send email to {0} failed".format(email_adr))
+            return False
+    
+    def recv_mail(self):
+        if not self.downloader:
+            print("downloader not login")
+            return False
+    
+        self.downloader.select() # Select inbox or default namespace
+        (retcode, messages) = self.downloader.search(None, '(UNSEEN)')
+        if retcode != 'OK':
+            print("read mail from server failed")
+            return False
+    
+        if not os.path.exists("./downmail"):
+            os.mkdir("./downmail")
+    
+        for num in messages[0].split():
+            print('Processing: {0}'.format(num))
+            typ, data = self.downloader.fetch(num.decode('utf-8'),'(RFC822)')
+            # 标记已读
+            sr = self.downloader.store(num, '+FLAGS', '\Seen')
+            # 标记删除
+            #sr = downloader.store(num, '+FLAGS', '\\Deleted')
+            email_message = email.message_from_string(data[0][1].decode('utf-8'))
+    
+            # mail to string
+            fp = StringIO()
+            g = generator.Generator(fp, mangle_from_=True, maxheaderlen=60)
+            g.flatten(email_message)
+            email_text = fp.getvalue()
+    
+            # mail_string to json_string
+            pmail = mailparser.parse_from_string(email_text)
+            email_json = pmail.mail_json
+            # mail to json obj
+            email_data = json.loads(email_json)
+    
+            # 处理邮件
+            self.handle_mail(email_data)
+    
+            subject = email_data.get('subject')
+            body = email_data.get('body')
+            print("get mail: subject[{0}] body.size[{1}]".format(subject, len(body)))
+    
+            filename = './downmail/{0}.eml'.format(subject)
+            with open(filename, 'w') as fout:
+                gr = generator.Generator(fout)
+                gr.flatten(email_message)
+                #fout.write(email_text)
+                fout.close()
+    
+            filename_j = './downmail/{0}.json'.format(subject)
+            with open(filename_j, 'w') as fjout:
+                fjout.write(json.dumps(email_data, indent=4, ensure_ascii=False))
+                fjout.close()
+    
+            print("save {0} ok,\n".format(filename))
+    
+        return True
+
+    def recv_mail_weibo(self, subject_pattern, from_email_pattern):
+        if not self.downloader:
+            print("downloader not login")
+            if not self.login():
+                return
+    
+        self.downloader.select() # Select inbox or default namespace
+        (retcode, messages) = self.downloader.search(None, '(UNSEEN)')
+        if retcode != 'OK':
+            print("read mail from server failed")
+            return False
+    
+        for num in messages[0].split():
+            print('Processing: {0}'.format(num))
+            typ, data = self.downloader.fetch(num.decode('utf-8'),'(RFC822)')
+            email_message = email.message_from_string(data[0][1].decode('utf-8'))
+    
+            # mail to string
+            fp = StringIO()
+            g = generator.Generator(fp, mangle_from_=True, maxheaderlen=60)
+            g.flatten(email_message)
+            email_text = fp.getvalue()
+    
+            # mail_string to json_string
+            pmail = mailparser.parse_from_string(email_text)
+            email_json = pmail.mail_json
+            # mail to json obj
+            email_data = json.loads(email_json)
+            subject = email_data.get('subject')
+            if subject.find(subject_pattern) == -1:
+                continue
+            from_email = email_data.get('from')[0] # list
+            if from_email_pattern not in from_email:
+                continue
+
+            # 标记已读
+            sr = self.downloader.store(num, '+FLAGS', '\Seen')
+            # 标记删除
+            #sr = downloader.store(num, '+FLAGS', '\\Deleted')
+            
+            # find target email
+            body = email_data.get('body')
+            print("read email: subject:{0} body size:{1}".format(subject, len(body)))
+            vcode = body.split('\n')[0]
+            return vcode
+    
+        return ''
+
+    
+    def handle_mail(self, email_data):
+        subject = email_data.get('subject')
+        body = email_data.get('body')
+        from_email = email_data.get('from')[0]
+        print("recv email from {0} subject is :{1}".format(from_email[1], subject))
+        # do something handle email
+        return
+    
+    def get_weibo_vcode(self, subject_pattern, from_email_pattern):
+        return self.recv_mail_weibo(subject_pattern, from_email_pattern)
+
+def main():
+    mbot = MailBot()
+    if not mbot.login(10):
+        print("login mail failed")
+        return
+
+    vcode = mbot.get_weibo_vcode("weibo_vcode", "nikinumber4@163.com")
+    print("get weibo vcode:{0}".format(vcode))
+
+if __name__ == "__main__":
+    main()
+
---
+++ topargus.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+import codecs
+import csv
+import os
+import random
+import copy
+import re
+import sys
+import traceback
+from collections import OrderedDict
+from datetime import datetime, timedelta
+import time
+import json
+import base64
+import threading
+from PIL import Image
+import string
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By 
+
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests_toolbelt import MultipartEncoder
+from urllib.parse import urlencode
+from urllib.parse import urlparse
+from lxml import etree
+from tqdm import tqdm
+from mailbot import MailBot
+from slogging import slog
+
+import pdb
+
+
+config = {}
+
+def decorator_try_except(func):
+    def new_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            slog.warn("Got error! {0}".format(repr(e)))
+            return 'EEE'
+    return new_func
+
+class TOPARGUS(object):
+    def __init__(self, username, password, host):                               
+        slog.info('TOPARGUS init')
+        self.username = username
+        self.password = password
+        self.host = host
+        self.url_prefix = 'http://{0}:{1}@{2}'.format(self.username, self.password, self.host)
+
+        # start mailbot ,try to login
+        self.mailbot = MailBot().login(10)
+        self.ss = requests.Session()
+        self.ss.auth = (username, password)
+        self.ss.mount('http://', HTTPAdapter(max_retries=3))
+        self.ss.mount('https://', HTTPAdapter(max_retries=3))
+
+        #_____________________启动参数___________________________
+        options = webdriver.ChromeOptions()
+        #options.add_argument('headless')
+        options.add_argument('disable-gpu')
+        options.add_argument("window-size=1220,1080")
+        options.add_argument("start-maximized")
+        options.add_argument("no-sandbox")
+
+        #_____________________代理参数___________________________
+        desired_capabilities = options.to_capabilities()
+        desired_capabilities['acceptSslCerts'] = True
+        desired_capabilities['acceptInsecureCerts'] = True
+        desired_capabilities['proxy'] = {
+            "httpProxy": config.get('PROXY'),
+            "ftpProxy": config.get('PROXY'),
+            "sslProxy": config.get('PROXY'),
+            "noProxy": None,
+            "proxyType": "MANUAL",
+            "class": "org.openqa.selenium.Proxy",
+            "autodetect": False,
+        }
+
+        #_____________________启动浏览器___________________________
+        self.driver = webdriver.Chrome(
+            options=options,
+            #executable_path=CHROME_DRIVER_PATH,
+            desired_capabilities = desired_capabilities)
+        self.driver.set_page_load_timeout(30)
+        return
+
+
+    def __del__(self):                                                                                         
+        slog.info('TOPARGUS uninit')
+        try:
+            """ Destroy the web browser """
+            self.driver.close()
+            self.driver.quit()
+        except Exception as e:
+            slog.warn("quit Exception: ", e)
+        finally:
+            return
+
+    def wait_for_ajax_data(self, wait = 0):                                                                              
+        """                                                                                                    
+        Waiting for the loading of ajax data filed to table
+        """                                                                                                    
+        try:                                                                                                   
+            WebDriverWait(self.driver, wait, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, 'xxxxx')))
+            return True                                                                                        
+        except:                                                                                                
+            return False                                                                                       
+
+    def randomString(self, stringLength=5):
+        """Generate a random string of fixed length """
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(stringLength))
+
+    @decorator_try_except
+    def load_url(self, url, wait = 0):
+        slog.info("访问 url:{0}".format(url))
+        try:                                                                                                   
+            self.driver.get(url)
+        except Exception as e:                                                                                 
+            slog.warn("url:{0} cannot be reached:{1}".format(url,e))
+            return False, None
+
+        if not os.path.exists('./temp'):
+            os.mkdir('./temp')
+        page = './temp/{0}.png'.format(self.randomString(10))
+        if wait != 0:
+            self.wait_for_ajax_data(wait)
+        self.driver.save_screenshot(page)
+        return True, page
+
+    def home(self):
+        slog.info("加载首页")
+        url = '{0}/index.html'.format(self.url_prefix)
+        return self.load_url(url = url)
+
+    def system(self, cpu = False, send_bandwidth = False, recv_bandwidth = False):
+        slog.info("加载系统情况页面")
+        if cpu == True:
+            url = '{0}/system.html?field=cpu'.format(self.url_prefix)
+            self.load_url(url = url)
+
+        if send_bandwidth == True:
+            url = '{0}/system.html?field=send_bandwidth'.format(self.url_prefix)
+            self.load_url(url = url)
+
+        if recv_bandwidth== True:
+            url = '{0}/system.html?field=recv_bandwidth'.format(self.url_prefix)
+            self.load_url(url = url)
+
+        return True
+
+    def alarm(self, low = False, middle = False, high = True, all_alarm = False):
+        slog.info("加载告警事件页面")
+        if all_alarm == True:
+            url = '{0}/alarm.html'.format(self.url_prefix)
+            return self.load_url(url = url)
+
+        if high == True:
+            url = '{0}/alarm.html?priority=2'.format(self.url_prefix)
+            return self.load_url(url = url)
+
+        if middle == True:
+            url = '{0}/alarm.html?priority=1'.format(self.url_prefix)
+            return self.load_url(url = url)
+
+        if low == True:
+            url = '{0}/alarm.html?priority=0'.format(self.url_prefix)
+            return self.load_url(url = url)
+        
+        return True
+
+    def packet(self, dest_node_id = None):
+        url = '{0}/packet.html'.format(self.url_prefix)
+        if dest_node_id:
+            url = '{0}/packet.html?dest_node_id={1}'.format(self.url_prefix, dest_node_id)
+        return self.load_url(url = url, wait = 30)
+
+    def network(self, network_id = None):
+        url = '{0}/network.html'.format(self.url_prefix)
+        if network_id:
+            url = '{0}/network.html?network_id={1}'.format(self.url_prefix, network_id)
+        return self.load_url(url = url, wait = 30)
+
+    def send_mail_with_verifycode_wait_answer(self):
+        # send vcode email to someone, wait answer
+        subject = "weibo_vcode_{0}".format(self.randomString(5))
+        contents = [
+                '微博需要登录啦啦!',
+                self.mailbot.make_pic_inline(self.weibo_vcode_png)
+                ]
+        self.mailbot.send_mail('nikinumber4@163.com', subject, contents)
+        slog.info("send verify code in mail to nikinumber@163.com, wait for answer...")
+        return
+
+    def go_top(self):
+        self.driver.execute_script('var q=document.documentElement.scrollTop=0')
+        time.sleep(2)
+
+    # 没有使用 selenium 
+    def alarm_api(self):
+        end = int(time.time() * 1000)
+        begin = end - 10 * 60 * 1000   # latest 10 min
+        url = 'http://{0}/api/web/system_alarm_info?public_ip_port=&priority=2&begin={1}&end={2}&limit=200&page=1'.format(self.host, begin, end)
+        my_headers = {
+                    'Accept': 'application/json, text/javascript, */*; q=0.01',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive',
+                    'Host': self.host,
+                    'Referer': self.url_prefix + '/index.html',
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    }
+        '''
+        {"error":"OK","results":{"size":0,"system_alarm_info":[]},"status":0,"total":0}
+        '''
+
+        results = None
+        try:
+            res = self.ss.get(url, headers = my_headers)
+            if res.status_code == 200:
+                results = res.json().get('results')
+        except Exception as e:
+            slog.warn("catch exception:{0}".format(e))
+
+        slog.debug("get result:{0}".format(json.dumps(results)))
+        return results
+
+    # 没有使用 selenium 
+    def node_info_api(self):
+        url = 'http://{0}/api/web/node_info/?status=offline'.format(self.host)
+        my_headers = {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive',
+                    'Host': self.host,
+                    'Referer': self.url_prefix + '/index.html',
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+                    }
+        '''
+        {"error":"OK","results":{"node_info":[],"node_size":0},"status":0}
+        '''
+
+        results = None
+        try:
+            res = self.ss.get(url, headers = my_headers)
+            if res.status_code == 200:
+                results = res.json().get('results')
+        except Exception as e:
+            slog.warn("catch exception:{0}".format(e))
+
+        slog.debug("get result:{0}".format(json.dumps(results)))
+        return results
+
+    # 没有使用 selenium 
+    def packet_drop_api(self):
+        end = int(time.time() * 1000)
+        begin = end - 10 * 60 * 1000   # latest 10 min
+        url = 'http://{0}/api/web/packet_drop/?begin={1}&end={2}'.format(self.host, begin, end)
+        my_headers = {
+                    'Accept': 'application/json, text/javascript, */*; q=0.01',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive',
+                    'Host': self.host,
+                    'Referer': self.url_prefix + '/index.html',
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    }
+        '''
+        {"error":"OK","results":[[1579441355398,1.8],[1579441415398,3.6],[1579441475398,0.0]],"status":0}
+        '''
+
+        results = None
+        try:
+            res = self.ss.get(url, headers = my_headers)
+            if res.status_code == 200:
+                results = res.json().get('results')
+        except Exception as e:
+            slog.warn("catch exception:{0}".format(e))
+
+        slog.debug("get result:{0}".format(json.dumps(results)))
+        return results
+
+
+
+
+@decorator_try_except
+def run_api(topargus):
+    '''
+    def alarm_api(self):
+    def node_info_api(self):
+    def packet_drop_api(self):
+    '''
+    slog.debug("run_api alive")
+
+    results = topargus.alarm_api()
+    if results:
+        if results.get('system_alarm_info'):
+            slog.warn('get alarm high level info')
+            ret = topargus.alarm(high = True)  # (True, filename)
+
+    results = topargus.node_info_api()
+    if results:
+        if results.get('node_info'):
+            slog.warn('get offline node_info')
+            # TODO
+    
+    # results = topargus.packet_drop_api()
+
+    return True
+
+def th_api(topargus):
+    while True:
+        #time.sleep(10 * 60)  # 10 min
+        time.sleep(10)  # 10 min
+        run_api(topargus)
+    return
+
+def th_page(topargus):
+    while True:
+        #time.sleep(60 * 60)  # 60 min
+        time.sleep(1 * 60)  # 60 min
+        ret = topargus.home()
+        ret = topargus.alarm()
+        ret = topargus.packet()
+        ret = topargus.network()
+    return
+
+
+
+
+def main():
+    username = 'test'
+    password = 'test'
+    host = '192.168.50.242'
+    topargus = TOPARGUS(username = username, password = password, host = host)
+
+    api_th = threading.Thread(target = th_api, args = (topargus, ))
+    api_th.start()
+    slog.info('api thread start')
+
+    page_th = threading.Thread(target = th_page, args = (topargus, ))
+    page_th.start()
+    slog.info('page thread start')
+
+    print("TOPARGUS 系统监控中...")
+    slog.info("TOPARGUS 系统监控中...")
+    while True:
+        time.sleep(1)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
