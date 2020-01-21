3b7c7f4b95bde4a3cf31750856ae16a3496bc470
smaugx <linuxcode2niki@gmail.com>
2020-01-21 16:08:48 +0800

    change window site more lager

--- topargus.py
+++ topargus.py
@@ -60,7 +60,7 @@ class TOPARGUS(object):
         options.add_argument('headless')
         options.add_argument('disable-gpu')
         #options.add_argument("window-size=1220,1080")
-        options.add_argument("window-size=1440,1080")
+        options.add_argument("window-size=1440,1280")
         options.add_argument("start-maximized")
         options.add_argument("no-sandbox")
 
